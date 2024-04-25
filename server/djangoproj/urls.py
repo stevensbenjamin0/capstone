22155f3a6364dbbf00852d6c793e2bee4a687637
Ben Stevens <146244179+stevensbenjamin0@users.noreply.github.com>
2024-04-25 18:56:00 -0400

    Update urls.py
    created path for contact page

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('djangoapp/', include('djangoapp.urls')),
     path('', TemplateView.as_view(template_name="Home.html")),
     path('About', TemplateView.as_view(template_name="About.html")),
+    path('Contact', TemplateView.as_view(template_name="Contact.html")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
