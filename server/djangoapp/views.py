3a5d5a6a19b07c961a06c0e07eebcedfa5591ecc
Ben Stevens <146244179+stevensbenjamin0@users.noreply.github.com>
2024-04-29 17:49:02 -0400

    Update views.py
    added get_cars view

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -75,6 +75,18 @@ def registration(request):
         data = {"userName":username,"error":"Already Registered"}
         return JsonResponse(data)
 
+# Create a 'get_cars' view to get a list of cars
+def get_cars(request):
+    count = CarMake.objects.filter().count()
+    print(count)
+    if(count == 0):
+        initiate()
+    car_models = CarModel.objects.select_related('car_make')
+    cars = []
+    for car_model in car_models:
+        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+    return JsonResponse({"CarModels":cars})
+
 # # Update the `get_dealerships` view to render the index page with
 # a list of dealerships
 # def get_dealerships(request):
