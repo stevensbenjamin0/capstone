ee6268fb824e28a64eb0b56da979a648a154e7a0
Ben Stevens <146244179+stevensbenjamin0@users.noreply.github.com>
2024-05-07 14:35:27 -0400

    Update App.js

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,6 +1,7 @@
 import LoginPanel from "./components/Login/Login"
 import Register from "./components/Register/Register"
 import { Routes, Route } from "react-router-dom";
+import Dealer from "./components/Dealers/Dealer";
 import Dealers from './components/Dealers/Dealers';
 
 function App() {
